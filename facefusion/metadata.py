91f3b3095ae543c8fc58651a30647cd336edc986
Acces Forbidden <accesforbidden@gmail.com>
2024-05-07 20:01:13 +0700

    Add files via upload

---
+++ facefusion/metadata.py
@@ -0,0 +1,13 @@
+METADATA =\
+{
+	'name': 'FakeYourDreamz Premium Software',
+	'description': 'Next generation face swapper and enhancer',
+	'version': '2.5.2',
+	'license': 'MIT',
+	'author': 'FakeYourDreamz',
+	'url': 'https://t.me/FakeYourDreamz'
+}
+
+
+def get(key : str) -> str:
+	return METADATA[key]
